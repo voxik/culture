183973b78ba278d379c83c7ead54436a13d149f3
//de <de@freed.network>
2015-04-29 12:10:42 -0700

    beefed up reloading

--- sync.rb
+++ sync.rb
@@ -1,41 +1,49 @@
 module Celluloid
   module Sync
 
-    @@gem_path ||= File.expand_path("../../", __FILE__)
-    $:.push( @@gem_path)
+    unless defined? @@updated
+      @@gem_path ||= File.expand_path("../../", __FILE__)
+      $:.push( @@gem_path)
 
-    puts "Synchronizing Celluloid Culture //"
-    @@update = `cd #{@@gem_path}/culture; git pull`
-    @@updated = @@update.include?("up-to-date")
-    @@required ||= ["#{@@gem_path}/culture/sync"]
-    
-    require(@@required << File.expand_path("../gems/loader", __FILE__))
-    GEM = Celluloid::Gems::SELF unless defined? GEM
+      puts "Synchronizing Celluloid Culture //"
+      @@update = %x[cd #{@@gem_path}/culture; git pull]
+      @@updated = @@update.include?("up-to-date")
 
-    LIB_PATH = File.expand_path("../../lib/#{GEM.split("-").join("/")}", __FILE__)
-    if File.exist?(version="#{LIB_PATH}/version.rb")
-      require(version)
+      @@required ||= [
+        "#{@@gem_path}/culture/sync",
+        "#{@@gem_path}/culture/gems/loader"
+      ]
     end
 
-    class << self
+    unless @@updated
+      require(@@required.last)
+      GEM = Celluloid::Gems::SELF unless defined? GEM
 
-      def update!
-        if @@updated
-          puts "Celluloid Culture was updated."
-          @@required.each { |rb| load(rb) }
-          puts "Reloaded Culture::Sync itself."
-        end
+      LIB_PATH = File.expand_path("../../lib/#{GEM.split("-").join("/")}", __FILE__)
+      if File.exist?(version="#{LIB_PATH}/version.rb")
+        require(version)
       end
 
-      def gems(loader)
-        case loader.class
-        when Gem::Specification
-          Gems.gemspec(loader)
-        when Bundler::Dsl
-          Gems.bundler(loader)
-        end
-      end
+      class << self
 
+        def update!
+          if @@updated
+            puts "Celluloid Culture was updated."
+            @@required.each { |rb| load(rb) }
+            puts "Reloaded Culture::Sync itself."
+          end
+        end
+
+        def gems(loader)
+          case loader.class
+          when Gem::Specification
+            Gems.gemspec(loader)
+          when Bundler::Dsl
+            Gems.bundler(loader)
+          end
+        end
+
+      end
     end
   end
 end
