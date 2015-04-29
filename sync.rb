21ec70ad7e11ad0979fa3401e29efb1c4935eaf7
//de <de@freed.network>
2015-04-29 12:24:30 -0700

    nix infinite loop

--- sync.rb
+++ sync.rb
@@ -6,49 +6,52 @@ module Celluloid
       $:.push( @@gem_path)
 
       puts "Synchronizing Celluloid Culture //"
-      @@update = %x[cd #{@@gem_path}/culture; git pull]
+      @@update = `cd #{@@gem_path}/culture; git pull`
       @@updated = !@@update.include?("up-to-date")
 
       @@required ||= [
         "#{@@gem_path}/culture/sync.rb",
         "#{@@gem_path}/culture/gems/loader"
       ]
-      puts @@update
     end
 
     class << self
-      def update!
-        if @@updated
-          puts "Celluloid Culture was updated."
-          @@required.each { |rb| load(rb) }
-          puts "Reloaded Culture::Sync itself."
-        end
+      def updated?
+        @@updated
       end
-    end
-
-    unless @@updated
-      require(@@required.last)
-      GEM = Celluloid::Gems::SELF unless defined? GEM
-
-      LIB_PATH = File.expand_path("../../lib/#{GEM.split("-").join("/")}", __FILE__)
-      if File.exist?(version="#{LIB_PATH}/version.rb")
-        require(version)
-      end
-
-      class << self
-
-        def gems(loader)
-          case loader.class
-          when Gem::Specification
-            Gems.gemspec(loader)
-          when Bundler::Dsl
-            Gems.bundler(loader)
+      if @@updated
+        def update!
+          if @@updated
+            puts "Celluloid Culture was updated."
+            @@required.each { |rb| load(rb) }
+            puts "Reloaded Culture::Sync itself:\n#{@@update}"
           end
         end
+      end
+      else
+        require(@@required.last)
+        GEM = Celluloid::Gems::SELF unless defined? GEM
 
+        LIB_PATH = File.expand_path("../../lib/#{GEM.split("-").join("/")}", __FILE__)
+        if File.exist?(version="#{LIB_PATH}/version.rb")
+          require(version)
+        end
+
+        class << self
+
+          def gems(loader)
+            case loader.class
+            when Gem::Specification
+              Gems.gemspec(loader)
+            when Bundler::Dsl
+              Gems.bundler(loader)
+            end
+          end
+
+        end
       end
     end
   end
 end
 
-Celluloid::Sync.update!
+Celluloid::Sync.update! if Celluloid::Sync.updated?
