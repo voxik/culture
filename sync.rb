3bbd8759b8a9a4e4e5ba7027c58687f9be8d3249
//de <de@freed.network>
2015-04-29 12:41:50 -0700

    ditch the fancy shit

--- sync.rb
+++ sync.rb
@@ -10,16 +10,14 @@ module Celluloid
     @@update = `cd #{@@gem_path}/culture; git pull`
     @@updated = !@@update.include?("up-to-date")
 
-    @@require ||= [ "#{@@gem_path}/culture/gems/loader" ]
-
-    if File.exist?(version="#{LIB_PATH}/version.rb")
-      @@require << version
-    end
-
-    @@require.each { |rb| require(rb) }
+    require("#{@@gem_path}/culture/gems/loader")
 
     GEM = Celluloid::Gems::SELF unless defined? GEM
     LIB_PATH = File.expand_path("../../lib/#{GEM.split("-").join("/")}", __FILE__)
+
+    if File.exist?(version="#{LIB_PATH}/version.rb")
+      require(version)
+    end
 
   end
 end
