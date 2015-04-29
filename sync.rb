7e48847dc5423155170a1dd20e20d24d167124eb
//de <de@freed.network>
2015-04-29 12:48:38 -0700

    rubocop

--- sync.rb
+++ sync.rb
@@ -1,23 +1,21 @@
 module Celluloid
   module Sync
-
-    @@gem_path ||= File.expand_path("../../", __FILE__)
-    $:.push( @@gem_path)
+    @gem_path ||= File.expand_path("../../", __FILE__)
+    $LOAD_PATH.push(@gem_path)
 
     # TODO: This will likely need to be done differently if INSIDE a cut gem.
 
     puts "Synchronizing Celluloid Culture //"
-    @@update = `cd #{@@gem_path}/culture; git pull`
-    @@updated = !@@update.include?("up-to-date")
+    @update = `cd #{@gem_path}/culture; git pull`
+    @updated = !@update.include?("up-to-date")
 
-    require("#{@@gem_path}/culture/gems/loader")
+    require("#{@gem_path}/culture/gems/loader")
 
     GEM = Celluloid::Gems::SELF unless defined? GEM
-    LIB_PATH = File.expand_path("../../lib/#{GEM.split("-").join("/")}", __FILE__)
+    LIB_PATH = File.expand_path("../../lib/#{GEM.split('-').join('/')}", __FILE__)
 
-    if File.exist?(version="#{LIB_PATH}/version.rb")
+    if File.exist?(version = "#{LIB_PATH}/version.rb")
       require(version)
     end
-
   end
 end
