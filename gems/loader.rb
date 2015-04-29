0ca0ba031dfad9615fa3c7e09f12ba6805872f41
//de <de@freed.network>
2015-04-29 12:33:00 -0700

    remove some debug output, tweak a bit

--- gems/loader.rb
+++ gems/loader.rb
@@ -19,40 +19,37 @@ module Celluloid
     end
     # /TODO
 
-    @@dependencies ||= nil
-    unless @dependencies
+    unless @@dependencies ||= nil
       @@dependencies = if File.exist?(GEMS)
         YAML.load_file(GEMS)
       end
     end
 
-    puts "dependencies: #{@@dependencies}"
-
     unless @@dependencies.is_a? Hash and @@dependencies.any?
       raise "Celluloid cannot find its dependencies."
     end
 
-    def load
-      @@dependencies.each do |name, spec|
+    def loader
+      @@dependencies.each{ |name, spec|
         next if name == SELF
         puts "Updating #{name}?"
         yield name, spec
-      end
+      }
     end
 
     def gemspec(gem)
-      Gems.load do |name, spec|
+      loader { |name, spec|
         req = spec["gemspec"] || []
         gem.add_dependency(name, *req)
-      end
+      }
     end
 
     def gemfile(dsl)
-      Gems.load do |name, spec|
+      loader { |name, spec|
         req = spec["bundler"] || {}
         req = req.each_with_object({}) { |(k, v), o| o[k.to_sym] = v }
         dsl.gem(name, req)
-      end
+      }
     end
   end
 end
