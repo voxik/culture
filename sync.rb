37e242a2539c80a48c69cdd66b16dc5defc775d8
//de <de@freed.network>
2015-04-29 11:44:01 -0700

    find paths

--- sync.rb
+++ sync.rb
@@ -5,7 +5,14 @@ module Celluloid
     $:.push( @@gem_path)
 
     require(File.expand_path("../gems/loader", __FILE__))
-    GEM = Celluloid::Gems::SELF
+    GEM = Celluloid::Gems::SELF unless defined? GEM
+
+    LIB_PATH = File.expand_path("../lib/#{GEM.split("-").join("/")}", __FILE__)
+    if File.exist?(version="#{LIB_PATH}/version.rb")
+      require(version)
+    end
+
+    puts "#{[LIB_PATH,lib]}"
 
     class << self
 
@@ -13,7 +20,6 @@ module Celluloid
 
         puts "loader? #{loader.class}"
         puts "git pull #{@gem_path}/culture"
-
 
       end
     end
