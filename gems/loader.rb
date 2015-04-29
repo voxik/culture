b6c2cefbe2068f28746dbbefcd424f4b0bb99c63
//de <de@freed.network>
2015-04-29 06:32:35 -0700

    adapt loader to pull from dependencies.yml

--- gems/loader.rb
+++ gems/loader.rb
@@ -4,32 +4,48 @@ module Celluloid
   module Gems
     extend self
 
-    DEFAULT_CFG = ".celluloid.yml"
+    # TODO: Better way to do this I'm sure.
+    SELF = begin
+      path = __FILE__.split("/")
+      path.pop(3)
+      path.last
+    end
 
-    def load(cfg)
-      return unless File.exist? cfg
-      cfg = YAML.load_file(cfg)
-      return unless cfg
-      cfg["gems"].each do |name, spec|
-        self? name
+    GEMS = begin
+      path = File.expand_path(__FILE__).split("/")
+      path.pop
+      path.push("dependencies.yml")
+      path.join("/")
+    end
+    # /TODO
+
+    @@dependencies ||= nil
+    unless @dependencies
+      @@dependencies = if File.exist?(GEMS)
+        YAML.load_file(GEMS)
+      end
+    end
+
+    unless @@dependencies.is_a? Hash and @@dependencies.any?
+      raise "Celluloid cannot find its dependencies."
+    end
+
+    def load
+      @@dependencies.each do |name, spec|
+        next if name == SELF
         yield name, spec
       end
     end
 
-    # Avoid gems loading themselves.
-    def self?(name)
-      puts "self? #{name} #{__FILE__}"
-    end
-
-    def gemspec(gem, cfg=DEFAULT_CFG)
-      Gems.load(cfg) do |name, spec|
+    def gemspec(gem)
+      Gems.load do |name, spec|
         req = spec["gemspec"] || []
         gem.add_dependency(name, *req)
       end
     end
 
-    def bundler(bundler, cfg=DEFAULT_CFG)
-      Gems.load(cfg) do |name, spec|
+    def bundler(bundler)
+      Gems.load do |name, spec|
         req = spec["bundler"] || {}
         req = req.each_with_object({}) { |(k, v), o| o[k.to_sym] = v }
         bundler.gem(name, req)
