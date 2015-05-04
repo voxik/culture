e82cea78b533bfa2b656978b3d4c89cf40849e48
//de <de@freed.network>
2015-05-03 23:16:56 -0700

    remove debug output

--- gems/loader.rb
+++ gems/loader.rb
@@ -36,10 +36,8 @@ module Celluloid
         req = spec["gemspec"] || []
         if ["core", "module"].include?(spec["dependency"]) 
           if core?
-            puts "core dependency: all are runtime: #{name}"
             gem.add_runtime_dependency(name, *req)
           else
-            puts "module dependency: all are development: #{name}"
             gem.add_development_dependency(name, *req)
           end
         else
