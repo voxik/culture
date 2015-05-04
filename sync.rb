a04c566144001634df5e91edd5e79ba6de8bd21d
//de <de@freed.network>
2015-05-03 19:16:16 -0700

    redo to use functions that can be undef'ed versus constants

--- sync.rb
+++ sync.rb
@@ -1,27 +1,40 @@
 module Celluloid
   module Sync
-    GEM_PATH ||= File.expand_path("../../", __FILE__)
-    $LOAD_PATH.push(GEM_PATH)
 
-    SELF = Dir["#{File.expand_path('../../', __FILE__)}/*.gemspec"].first.gsub(".gemspec", "").split("/").last
-    fail "Missing gemspec." unless SELF
+    class < self
+      def gem_path
+        File.expand_path("../../", __FILE__)
+      end
+      def gem_name
+        Dir["#{File.expand_path('../../', __FILE__)}/*.gemspec"].first.gsub(".gemspec", "").split("/").last
+      end
+      def gem_name?
+        !gem_name.nil?
+      end
+      def lib_path
+        File.expand_path("../../lib", __FILE__)
+      end
+      def lib_gempath
+        "#{lib_path}/#{gem_name.split('-').join('/')}"
+      end
+    end
+
+    fail "Missing gemspec." unless gem_name?
+    $LOAD_PATH.push(gem_path)
+    $LOAD_PATH.push(lib_path)
+
 
     # TODO: This will likely need to be done differently if INSIDE a cut gem.
 
     case File.basename($PROGRAM_NAME)
-    when "bundle", "rspec"
+    when "bundle"
+      puts $ARGV
       puts "Celluloid::Sync //"
-      `cd #{GEM_PATH}/culture; git pull origin master`
+      `cd #{gem_path}/culture; git pull origin master`
     end
 
-    require("#{GEM_PATH}/culture/gems/loader")
-
-    LIB_PATH = File.expand_path("../../lib", __FILE__)
-    LIB_GEMPATH = "#{LIB_PATH}/#{SELF.split('-').join('/')}"
-    $LOAD_PATH.push(LIB_PATH)
-
-    puts "#{LIB_GEMPATH}/version.rb"
-    if File.exist?(version = "#{LIB_GEMPATH}/version.rb")
+    require("#{gem_path}/culture/gems/loader")
+    if File.exist?(version = "#{lib_gempath}/version.rb")
       require(version)
     end
   end
