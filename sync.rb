48346bbdc9312e87e3c7ffbd4cec893cc02034df
//de <de@freed.network>
2015-05-05 20:49:57 -0700

    spaces required by rubocop, and undef where needed

--- sync.rb
+++ sync.rb
@@ -5,25 +5,33 @@ module Celluloid
       def gem_path
         File.expand_path("../../", __FILE__)
       end
+
       undef gem_name rescue nil
       def gem_name
         Dir["#{File.expand_path('../../', __FILE__)}/*.gemspec"].first.gsub(".gemspec", "").split("/").last
       end
+
       undef gem_name? rescue nil
       def gem_name?
         !gem_name.nil?
       end
+
       undef lib_path rescue nil
       def lib_path
         File.expand_path("../../lib", __FILE__)
       end
+
       undef lib_gempath rescue nil
       def lib_gempath
         "#{lib_path}/#{gem_name.split('-').join('/')}"
       end
+
+      undef scenario rescue nil
       def scenario
         File.basename($PROGRAM_NAME)
       end
+
+      undef bundler? rescue nil
       def bundler?
         scenario == "bundle"
       end
