2bec94b05e76f599b9172d47dd74bf1c2f591c21
//de <de@freed.network>
2015-05-05 22:04:05 -0700

    refactor as switch statement to reduce complexity

--- gems/loader.rb
+++ gems/loader.rb
@@ -13,6 +13,8 @@ module Celluloid
   end
   module Gems
     extend self
+    extend Forwardable
+    def_delegators :"Celluloid::Sync", :gem_name
 
     undef gems rescue nil
     def gems
@@ -27,30 +29,42 @@ module Celluloid
       fail "Celluloid cannot find its dependencies."
     end
 
-    def core?(name=Celluloid::Sync.gem_name)
-      return false unless @dependencies["#{Celluloid::Sync.gem_name}"].is_a? Hash
+    def core_gem?(name=gem_name)
+      return false unless @dependencies[gem_name].is_a? Hash
       @dependencies["#{name}"]["dependency"] == "core"
     end
 
+    # Returns true if this gem is *not* in the dependencies list at all,
+    # and the name of the gem passed in *is* the core gem.
     def needs_core?(name)
-      return false if @dependencies.keys.include? gem_name
-      core?(name)
+      return false if @dependencies.keys.include?(gem_name)
+      core_gem?(name)
     end
 
     def gemspec(gem)
       loader do |name, spec|
         req = spec["gemspec"] || []
-        if %w(core module).include?(spec["dependency"])
-          if core? || needs_core?(name)
-            gem.add_runtime_dependency(name, *req)
-          else
-            gem.add_development_dependency(name, *req)
-          end
-        elsif spec["dependency"] == "development"
-          gem.add_development_dependency(name, *req)
-        else
-          gem.add_dependency(name, *req)
-        end
+        # Rules for dependencies, to avoid so-called circular dependency:
+        # - Only the core gem lists all the modules as runtime depedencies.
+        # - If this gem is not in the dependencies list, then it needs the core gem at runtime;
+        #   the module gems are development dependencies only.
+        # - If the dependency is a module, it is only a development dependency to other modules,
+        #   and even the core gem is a development dependency. It is not expected to be used alone.
+        meth = case spec["dependency"]
+               when "core", "module"
+                 # For the core gem, all modules are runtime dependencies.
+                 # For external gems, only the core gem is a runtime dependency.
+                 if core_gem? || needs_core?(name)
+                   :add_runtime_dependency
+                 else
+                   :add_development_dependency
+                 end
+               when "development"
+                 :add_development_dependency
+               else
+                 :add_dependency
+               end
+        gem.send(meth, name, *req)
       end
     end
 
@@ -70,7 +84,7 @@ module Celluloid
 
     def loader
       @dependencies.each do |name, spec|
-        next if name == Celluloid::Sync.gem_name
+        next if name == gem_name
         spec ||= {}
         yield name, spec
       end
