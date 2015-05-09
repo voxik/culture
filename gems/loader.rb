8cafdd29fc6cf2292867e673c538eb313132e278
//de <de@freed.network>
2015-05-08 21:12:47 -0700

    more debug output

--- gems/loader.rb
+++ gems/loader.rb
@@ -39,7 +39,7 @@ module Celluloid
 
     def core?(name=gem_name)
       return false unless @dependencies[name].is_a? Hash
-      puts "gem? #{name} ... #{@dependencies[name]["dependency"]}"
+      puts "gem? #{name.class} ... #{name} ... #{@dependencies[name]["dependency"]}"
       @dependencies[name]["dependency"] == "core"
     end
 
